2a0f486bd17c034aeadd8e45781276ab2c2bdb98
Fábio Vasconcelos <fabio.ac.vasconcelos@gmail.com>
2019-11-15 23:06:07 +0000

    [MENU] Simple menu using stack

--- menu/menu.cpp
+++ menu/menu.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include "menuLib.h"
 
 namespace Tester {
 
@@ -68,55 +69,7 @@ namespace Tester {
     };
 
     std::stack <TerminalMenu> MenuObject;
-
-    class Act_BillAcceptor : public IAction {
-    public:
-        void Run () override {
-            config.type = "Bill_Acceptor";
-            //Preciso de tipo de aceitador, país e path
-        }
-    };
-
-    class Act_Printer : public IAction {
-    public:
-        void Run () override {
-            config.type = "Printer";
-            //Preciso de tipo de impressora e path
-        }
-    };
-
-    class Act_CardReader : public IAction {
-    public:
-        void Run () override {
-            config.type = "Card_Reader";
-            //Preciso de tipo de cardreader e path
-        }
-    };
-
-    class Act_RFID : public IAction {
-    public:
-        void Run () override {
-            config.type = "RFID";
-            //Preciso de tipo de rfid
-        }
-    };
-
-    class ExitAction : public IAction {
-    public:
-        void Run () override {
-            std::cout << "Bye Bye" << std::endl;
-            std::exit(0);
-        }
-    };
-
-    TerminalMenu const MainMenu ({
-        { "Bill Acceptor", new Act_BillAcceptor () },
-        { "Printer", new Act_Printer () },
-        { "Card Reader", new Act_CardReader () },
-        { "RFID", new Act_RFID () },
-        { "Exit", new ExitAction () }
-    });
- 
+    
     class Act_Enable : public IAction {
     public:
         void Run () override {
@@ -145,13 +98,21 @@ namespace Tester {
             std::cout << "Back" << std::endl;
         }
     };
-
+    
     TerminalMenu const BillAcceptorMenu ({
         { "Enable Bill", new Act_Enable () },
         { "Disable Bill", new Act_Disable () },
         { "Accept Bill", new Act_Accept () },
         { "Back", new BackAction () }
     });
+
+    class Act_BillAcceptor : public IAction {
+    public:
+        void Run () override {
+            config.type = "Bill_Acceptor";
+            MenuObject.push(BillAcceptorMenu);
+        }
+    };
 
     TerminalMenu const PrinterMenu ({
         { "Enable Printer", new Act_Enable () },
@@ -160,18 +121,58 @@ namespace Tester {
         { "Back", new BackAction () }
     });
 
+    class Act_Printer : public IAction {
+    public:
+        void Run () override {
+            config.type = "Printer";
+            MenuObject.push(PrinterMenu);
+        }
+    };
+
     TerminalMenu const CardReaderMenu ({
         { "Enable Card", new Act_Enable () },
         { "Disable Card", new Act_Disable () },
         { "Accept Card", new Act_Accept () },
         { "Back", new BackAction () }
     });
+    
+    class Act_CardReader : public IAction {
+    public:
+        void Run () override {
+            config.type = "Card_Reader";
+            MenuObject.push(CardReaderMenu);
+        }
+    };
 
     TerminalMenu const RFIDMenu ({
         { "Enable RFID", new Act_Enable () },
         { "Disable RFID", new Act_Disable () },
         { "Accept RFID", new Act_Accept () },
         { "Back", new BackAction () }
+    });
+
+    class Act_RFID : public IAction {
+    public:
+        void Run () override {
+            config.type = "RFID";
+            MenuObject.push(CardReaderMenu);
+        }
+    };
+
+    class ExitAction : public IAction {
+    public:
+        void Run () override {
+            std::cout << "Bye Bye" << std::endl;
+            std::exit(0);
+        }
+    };
+
+    TerminalMenu const MainMenu ({
+        { "Bill Acceptor", new Act_BillAcceptor () },
+        { "Printer", new Act_Printer () },
+        { "Card Reader", new Act_CardReader () },
+        { "RFID", new Act_RFID () },
+        { "Exit", new ExitAction () }
     });
 }
 
