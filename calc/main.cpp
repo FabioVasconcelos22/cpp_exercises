19c181421f51e339d7839b72f8a8535a312fa4b5
Fábio Vasconcelos <fabio.vasconcelos@fabamaq.com>
2019-12-12 14:54:22 +0000

    [Calc] Logger to Calc

--- calc/main.cpp
+++ calc/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <string.h>
+#include <fstream>
 
 class IOperation {
 public:
-    virtual int Calc (int a, int b) = 0;
+    virtual float Calc (int a, int b) = 0;
 };
 
 class Sum: public IOperation{
 public:
-    int Calc(int a, int b) override
+    float Calc(int a, int b) override
     {
         return a + b;
     }
@@ -15,7 +17,7 @@ public:
 
 class Subtraction: public IOperation{
 public:
-    int Calc(int a, int b) override
+    float Calc(int a, int b) override
     {
         return a - b;
     }
@@ -23,7 +25,7 @@ public:
 
 class Multiplication: public IOperation{
 public:
-    int Calc(int a, int b) override
+    float Calc(int a, int b) override
     {
         return a * b;
     }
@@ -31,7 +33,7 @@ public:
 
 class Division: public IOperation{
 public:
-    int Calc(int a, int b) override
+    float Calc(int a, int b) override
     {
         return a / b;
     }
@@ -55,22 +57,75 @@ IOperation* getOperation(char op)
     {
         std::cout << "Not defined" << std::endl;
     }
-    
 }
+
+class ILogger {
+public:
+    virtual void Log (float result) = 0;
+};
+
+class SaveFile: public ILogger{
+public:
+    void Log(float result) override 
+    {
+        //Open file
+        std::ofstream ResultFile("Result.txt");
+        
+        //Write result
+        ResultFile << result;
+        
+        //close file
+        ResultFile.close();
+
+        std::cout << "Saved to File" << std::endl;
+    }
+};
+
+class TerminalPrint: public ILogger{
+public:
+    void Log(float result) override 
+    {
+        std::cout << "Result: " << result << std::endl;
+    }
+};
+
+ILogger* getLogger(int i)
+{
+    if( i==0 )
+    {
+        return new TerminalPrint;
+    }
+    else if ( i==1 )
+    {
+        return new SaveFile;
+    }
+    else
+    {
+        std::cout << "Logger not defined" << std::endl;
+    }
+};
 
 
 int main (int arg_c, char ** arg_v) {
     
+    int file = 0;
+
     // parse
     int a = std::stoi(arg_v[1]);
     int b = std::stoi(arg_v[2]);
     char opId = arg_v[3][0];
 
+    if(arg_c == 5 && strcmp(arg_v[4],"-f") == 0)
+    {
+        file = 1;
+    }
+
     // get calc
     auto * op = getOperation (opId);
+   
+    auto * log = getLogger (file);
 
-    std::cout << "Result: " << op->Calc (a, b) << std::endl;
-
+    log->Log(op->Calc(a,b));
 
     return 0;
 }
